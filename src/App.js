24e590a168cd026f7ede50b158dfc360a8536a03
Hung Tsou <hungtsougg@gmail.com>
2018-03-11 18:51:57 -0600

    enable project config files and css modules

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Classes from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={Classes.App}>
+        <header className={Classes.App_header}>
+          <img src={logo} className={Classes.App_logo} alt="logo" />
           <h1 className="App-title">Welcome to the burger builder</h1>
         </header>
         <p className="App-intro">
